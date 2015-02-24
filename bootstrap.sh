b60ec47ad124fd9fac22a667e57429877303358c
Tony Boyles <tboyles@sosacorp.com>
2015-02-24 15:00:24 -0500

    Rewrote bootstrap to be more concise, provide better respect to user privileges.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,69 +1,55 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' |
-tee /etc/apt/sources.list.d/mongodb.list
-
-sudo apt-get update
-
-echo "Installing base packages..."
-sudo apt-get install zlib1g-dev
-sudo apt-get install git <<-EOF
-yes
-EOF
-sudo apt-get install g++ <<-EOF
-yes
-EOF
-sudo apt-get install default-jre <<-EOF
-yes
-EOF
-sudo apt-get install zip
-sudo apt-get install unzip
-sudo apt-get install libxml2-dev <<-EOF
-yes
-EOF
-sudo apt-get install libxslt1-dev <<-EOF
-yes
-EOF
-sudo apt-get install python-dev <<-EOF
-yes
-EOF
-sudo apt-get install python-pip <<-EOF
-yes
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get -qq update 
+ 
+echo "Installing Base Dependencies..."
+sudo apt-get -qq install zip unzip zlib1g-dev git g++ default-jre libxml2-dev libxslt1-dev python-dev python-pip mongodb-10gen <<-EOF
+Y
 EOF
 
-echo "Cloning Phoenix pipeline files..."
-sudo git clone https://github.com/openeventdata/phoenix_pipeline.git
-sudo git clone https://github.com/openeventdata/scraper.git
-sudo git clone https://github.com/openeventdata/stanford_pipeline.git
-
+echo "Downloading Phoenix Pipeline..."
+echo " - Phoenix Pipeline..."
+git clone -q https://github.com/openeventdata/phoenix_pipeline.git
+echo " - Scraper..."
+git clone -q https://github.com/openeventdata/scraper.git
+echo " - Stanford Pipeline..."
+git clone -q https://github.com/openeventdata/stanford_pipeline.git
+echo " - CoreNLP..."
+wget -nv http://nlp.stanford.edu/software/stanford-corenlp-full-2014-06-16.zip
+unzip -qq stanford-corenlp-full-2014-06-16.zip
+rm stanford-corenlp-full-2014-06-16.zip
+mv stanford-corenlp-full-2014-06-16 stanford-corenlp
+cd stanford-corenlp
+wget -nv http://nlp.stanford.edu/software/stanford-srparser-2014-07-01-models.jar
+cd ..
+ 
 echo "Installing Python dependencies..."
-sudo pip install -r scraper/requirements.txt
-sudo pip install -r phoenix_pipeline/requirements.txt
-sudo pip install -r stanford_pipeline/requirements.txt
-
-echo "Installing PETRARCH..."
-sudo pip install git+https://github.com/openeventdata/petrarch.git
-cd
-
-echo "Downloading CoreNLP..."
-sudo wget http://nlp.stanford.edu/software/stanford-corenlp-full-2014-06-16.zip
-sudo unzip stanford-corenlp-full-2014-06-16.zip
-mv stanford-corenlp-full-2014-06-16 /home/vagrant/stanford-corenlp
-cd /home/vagrant/stanford-corenlp
-echo "Downloading shift-reduce parser..."
-sudo wget http://nlp.stanford.edu/software/stanford-srparser-2014-07-01-models.jar
-
+echo " - Scraper"
+sudo pip -q install -r scraper/requirements.txt
+echo " - PETRARCH..."
+sudo pip -q install git+https://github.com/openeventdata/petrarch.git
+echo " - Phoenix Pipeline..."
+sudo pip -q install -r phoenix_pipeline/requirements.txt
+echo " - Stanford Pipeline..."
+sudo pip -q install -r stanford_pipeline/requirements.txt
+ 
 echo "Downloading NLTK data..."
-sudo mkdir -p nltk_data/tokenizers
+mkdir -p nltk_data/tokenizers
 cd nltk_data/tokenizers
-sudo wget http://www.nltk.org/nltk_data/packages/tokenizers/punkt.zip
-sudo unzip punkt.zip
-cd
+wget -nv http://www.nltk.org/nltk_data/packages/tokenizers/punkt.zip
+unzip -qq punkt.zip
+cd ../..
 sudo mv nltk_data /usr/lib/nltk_data
+ 
+echo "Automating Jobs..."
+DIR=`pwd`
+CRONJOBS="@hourly cd $DIR/scraper && /usr/bin/python $DIR/scraper/scraper.py >> $DIR/scraper_stdout.log 2>&1
+0 1 * * * cd $DIR/phoenix_pipeline && /usr/bin/python $DIR/phoenix_pipeline/pipeline.py >> $DIR/pipeline_stdout.log 2>&1
+10 */3 * * * cd $DIR/stanford_pipeline && /usr/bin/python $DIR/stanford_pipeline/process.py >> $DIR/stanford_stdout.log 2>&1
+"
+ 
+echo "$CRONJOBS" > crontab.txt
+sudo crontab crontab.txt
 
-echo "Installing MongoDB..."
-sudo apt-get install mongodb-10gen
-
-echo "Setting up crontab..."
-sudo crontab /vagrant/crontab.txt
